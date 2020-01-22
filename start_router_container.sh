d7238b2cc344223f7a0be782a515b29714296b31
alexazarh <aazarh@gmail.com>
2020-01-22 14:29:17 -0600

    Update start_router_container.sh

--- start_router_container.sh
+++ start_router_container.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-docker network create \
-  --driver=bridge \
-  --subnet=172.16.12.0/24 \
-  --ip-range=172.16.12.0/24 \
-  --gateway=172.16.12.254 \
-  r1-r2
-docker container run --privileged --name=r1 --hostname=r1 --network=r1-r2 -itd alpine
+docker container run --privileged --name=$CONTAINER_NAME --hostname=$CONTAINER_NAME --network=$NETWORK_NAME -itd alpine

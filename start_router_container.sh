3b05fed2b4043ca7f2d865d5279f55437ddb4469
alexazarh <aazarh@gmail.com>
2020-01-22 14:42:20 -0600

    Update start_router_container.sh

--- start_router_container.sh
+++ start_router_container.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 docker container run --privileged --name=$CONTAINER_NAME --hostname=$CONTAINER_NAME --network=$NETWORK_NAME -itd alpine
+if [ $? -eq 0 ]
+then
+  echo "Container created"
+  exit 0
+else
+  echo "Container probably exists, starting container
+  docker start $CONTAINER_NAME
+  exit 0
+fi

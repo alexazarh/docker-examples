755a93295fa6f570029fae6eae6aa07f76907cbd
alexazarh <aazarh@gmail.com>
2020-01-22 11:23:06 -0600

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+docker network create \
+  --driver=bridge \
+  --subnet=172.16.12.0/24 \
+  --ip-range=172.16.12.0/24 \
+  --gateway=172.16.12.254 \
+  r1-r2
+docker container run --privileged --name=r1 --hostname=r1 --network=r1-r2 -itd alpine

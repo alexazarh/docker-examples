43c783c1ac6473c646dfe6cc3e61b0ed6bdaa7e5
alexazarh <aazarh@gmail.com>
2020-01-23 12:12:50 -0600

    Update create_or_start_router_group.sh

--- create_or_start_router_group.sh
+++ create_or_start_router_group.sh
@@ -6,7 +6,7 @@ docker network create r3-aggr
 
 for i in $(seq 1 5);do
 
-    for j in $(seq 1 5);do
+    for j in $(seq 1 3);do
 
         docker container run --privileged --name=$CONTAINER_GROUP_NAME-r$j-sw$i --hostname=r$j-sw$i --network=r$j-aggr -itd alpine
         if [ $? -eq 0 ]

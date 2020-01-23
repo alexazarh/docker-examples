f2b8f4033a6b6ad74b9be2f1759f9e1c33d89e9d
alexazarh <aazarh@gmail.com>
2020-01-23 09:51:13 -0600

    Create delete_group.sh

---
+++ delete_group.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+time for i in {0..4};do
+
+    time for j in {1..3};do
+      
+      docker stop $CONTAINER_GROUP_NAME-r$j-sw$i
+      docker rm $CONTAINER_GROUP_NAME-r$j-sw$i
+      
+    done
+    
+done

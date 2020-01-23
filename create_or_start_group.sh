c812c85dc78ef3828bdc451e9e785bab69f5452f
alexazarh <aazarh@gmail.com>
2020-01-23 10:08:37 -0600

    Update create_or_start_group.sh

--- create_or_start_group.sh
+++ create_or_start_group.sh
@@ -4,9 +4,9 @@ docker network create r1-aggr
 docker network create r2-aggr
 docker network create r3-aggr
 
-time for i in {0..4};do
+for i in {0..4};do
 
-    time for j in {1..3};do
+    for j in {1..3};do
 
         docker container run --privileged --name=$CONTAINER_GROUP_NAME-r$j-sw$i --hostname=r$j-sw$i --network=r$j-aggr -itd alpine
         if [ $? -eq 0 ]

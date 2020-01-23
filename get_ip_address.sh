f7bb1fc54bbb3ed4cd8ac10195c4f476722a6481
alexazarh <aazarh@gmail.com>
2020-01-23 11:08:38 -0600

    Update get_ip_address.sh

--- get_ip_address.sh
+++ get_ip_address.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker ps | egrep "$CONTAINER_NAME-.*" | awk '{print $1}' |  xargs -P0 -n 1  -I {} docker inspect --format='{{.Name}} {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' {} | sed 's/\///g' | head -n1 | awk '{print $2}'
+docker ps | egrep "$CONTAINER_NAME.*" | awk '{print $1}' |  xargs -P0 -n 1  -I {} docker inspect --format='{{.Name}} {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' {} | sed 's/\///g' | head -n1 | awk '{print $2}'

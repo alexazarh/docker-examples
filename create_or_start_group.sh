ff8637417d97099b6bf3934192a024d5ce6b85fe
alexazarh <aazarh@gmail.com>
2020-01-23 09:45:47 -0600

    Update create_or_start_group.sh

--- create_or_start_group.sh
+++ create_or_start_group.sh
@@ -5,7 +5,19 @@ docker network create r2-aggr
 docker network create r3-aggr
 
 time for i in {0..49};do
-    docker container run --privileged --name=$CONTAINER_GROUP_NAME-r1-sw$i --hostname=r1-sw$i --network=r1-aggr -itd alpine
-    docker container run --privileged --name=$CONTAINER_GROUP_NAME-r2-sw$i --hostname=r2-sw$i --network=r2-aggr -itd alpine
-    docker container run --privileged --name=$CONTAINER_GROUP_NAME-r3-sw$i --hostname=r3-sw$i --network=r3-aggr -itd alpine    
+
+    time for j in {1..3};do
+
+        docker container run --privileged --name=$CONTAINER_GROUP_NAME-r$j-sw$i --hostname=r$j-sw$i --network=r$j-aggr -itd alpine
+        if [ $? -eq 0 ]
+        then
+          echo "Container $CONTAINER_GROUP_NAME-r$j-sw$i created"
+        else
+          echo "Container exists, starting container $CONTAINER_GROUP_NAME-r$j-sw$i"
+          docker start $CONTAINER_GROUP_NAME-r$j-sw$i
+        fi
+    
+    done
 done
+
+exit 0

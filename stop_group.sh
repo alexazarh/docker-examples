4c92452302846b655f0ded391b3e2eeaeb02c96b
alexazarh <aazarh@gmail.com>
2020-01-23 10:14:31 -0600

    Update stop_group.sh

--- stop_group.sh
+++ stop_group.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-time for i in {0..4};do
-
-    time for j in {1..3};do
-      
-      docker stop $CONTAINER_GROUP_NAME-r$j-sw$i
-      
-    done
-    
-done
+docker ps | grep $CONTAINER_GROUP_NAME- | awk '{print $1}' | xargs -I {} -P 0 docker stop  {}
